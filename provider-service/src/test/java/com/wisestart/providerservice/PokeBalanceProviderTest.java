59fcc2360c28d2df62b2e6bdcefb958adf731f81
rafaelri <rafaelri@gmail.com>
2023-12-05 22:17:46 +0000

    first pact validation

---
+++ provider-service/src/test/java/com/wisestart/providerservice/PokeBalanceProviderTest.java
@@ -0,0 +1,40 @@
+package com.wisestart.providerservice;
+
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@Provider("PokeBalance")
+@PactFolder("pacts")
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PokeBalanceProviderTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget("localhost", port));
+    }
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void verifyPact(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+
+    @State("account 12345 with balance of 100 exists")
+    void account12345WithBalance100Exists() {
+
+    }
+}
